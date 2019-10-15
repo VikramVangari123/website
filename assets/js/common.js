9d6f10b87b6674ff2b92d6d9d5afbcbeacb3bb53
VikramVangari123 <42379080+VikramVangari123@users.noreply.github.com>
2019-10-15 16:49:19 +0530

    landscape and ow video

--- assets/js/common.js
+++ assets/js/common.js
@@ -615,23 +615,27 @@ if (screen.width > 767)
 {
     js.src = "../assets/page-scrolling/scroll.js";
 }
-else (screen.width < 768)
+else if (screen.width < 768)
 {
     js.src = "../assets/page-scrolling/scroll_m.js";
 }
-var query = window.matchMedia("(orientation:landscape)");
-var toggleGame = function(query) {
-    if (query.matches) {
-        js.src = "../assets/page-scrolling/scroll_m.js";
-    }
+else ((screen.width > 480) && (screen.height < 500 ))
+{
+    js.src = "../assets/page-scrolling/scroll_m.js";
 }
+// var query = window.matchMedia("(orientation:landscape)");
+// var toggleGame = function(query) {
+//     if (query.matches) {
+//         js.src = "../assets/page-scrolling/scroll_m.js";
+//     }
+// }
 head.appendChild(js);  
-if($('.ow-videos-in').click(function(){ 
-    $('.our-work').removeClass('active');
-}));
-else ($('.lity-close').click(function(){ 
-    $('.our-work').addClass('active');
-}));
+// if($('.ow-videos-in').click(function(){ 
+//     $('.our-work').removeClass('active');
+// }));
+// else ($('.lity-close').click(function(){ 
+//     $('.our-work').addClass('active');
+// }));
 
 // $('.ow-videos-in').on('click', function(){
 //     $('.section').removeClass('active');
